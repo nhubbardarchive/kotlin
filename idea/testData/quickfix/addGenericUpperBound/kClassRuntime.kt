e000e45f646c12df340108a6810cfbd7c3c45665
Stanislav Erokhin <Stanislav.Erokhin@jetbrains.com>
2017-01-26 01:37:09 +0300

    Make property `java` available on KClass<T> where T is type parameter without upper bounds.

--- idea/testData/quickfix/addGenericUpperBound/kClassRuntime.kt
+++ idea/testData/quickfix/addGenericUpperBound/kClassRuntime.kt
@@ -1,3 +1,5 @@
-// "Add 'kotlin.Any' as upper bound for E" "true"
+// "Add 'kotlin.Any' as upper bound for E" "false"
+// ACTION: Convert to block body
+// ACTION: Introduce local variable
 
 inline fun <reified /* abc */   E> bar() = E::class.java<caret>
